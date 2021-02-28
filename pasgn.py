5f97abdb45e0aecadecf2593d2844cbd1165f35b
patrickedwindaniel <74054171+patrickedwindaniel@users.noreply.github.com>
2021-02-28 14:34:48 +0530

    Add files via upload

---
+++ pasgn.py
@@ -0,0 +1,9 @@
+import random
+import json
+
+password = open('/home/stromefly/Desktop/pass.json')
+genre = password.read()
+obj = json.loads(genre)
+#length = 16
+raw = "".join(random.shuffle(obj,))
+print(raw)
